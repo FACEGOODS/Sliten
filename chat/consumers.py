5392af49d70bffe713b7f159bcbde09721e1f93e
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2021-01-31 20:56:45 +0000

    initial local commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,4 +6,4 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']
+        self.room_name = self.scope['url_route']['kwargs']['room_name'
