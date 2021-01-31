614126506ee56c0b754c4cd118596b7d4f062062
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2021-01-31 11:04:53 +0000

    Fix indent

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,4 +6,4 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self
+        self.room_name = self.scope['url_route']
