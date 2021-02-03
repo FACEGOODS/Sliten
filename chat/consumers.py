4aee7efa906d1e60b4993f9087a1f2ffc1ff6f91
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2021-02-03 18:17:29 +0000

    Add Gemfile.

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,4 +7,4 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.r
+        self.room_group_name = 
