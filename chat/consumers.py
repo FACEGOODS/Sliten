d05c18c536ec5d414b400cd3b78c1613c470f9b7
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2021-03-16 14:19:10 +0000

    Update list.json

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,4 +9,5 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room g
+        # Join room group
+        await self.ch
