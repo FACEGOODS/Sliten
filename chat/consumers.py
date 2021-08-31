24ad2aa069a9e990c58aafd9e7240aad2c1d54bf
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2021-08-31 11:26:42 +0000

    Update _style.scss

--- chat/consumers.py
+++ chat/consumers.py
@@ -46,4 +46,4 @@ class ChatConsumer(AsyncWebsocketConsumer):
                         'message': message
                     }
                 )
-            if send_to_type == 'C' or send
+            if send_to_type == 'C' or send_to_type == 'BC'
