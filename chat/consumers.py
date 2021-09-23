6e09579b4e4e92affa968a0d2a4479eebed3df99
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2021-09-23 16:23:01 +0000

    Add logs

--- chat/consumers.py
+++ chat/consumers.py
@@ -50,4 +50,4 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 await self.channel_layer.group_send(
                     'chat_C',
                     {
-              
+                        'type': 'chat_messag
