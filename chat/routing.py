43ed90282c92c9a11ed70ca48d2715b0734fbf33
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-01-06 14:03:51 +0000

    fix travis yml

--- chat/routing.py
+++ chat/routing.py
@@ -4,4 +4,4 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.
