463f0152653613624877d5bd4effacad019a434d
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-06-16 18:37:54 +0000

    example app

--- chat/tests.py
+++ chat/tests.py
@@ -32,4 +32,5 @@ class ChatTests(ChannelsLiveServerTestCase):
             self._switch_to_window(0)
             self._post_message('hello')
             WebDriverWait(self.driver, 2).until(lambda _:
-                'hello' in se
+                'hello' in self._chat_log_value,
+ 
