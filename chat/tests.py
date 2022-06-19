13de61957b5bb7130622e1fa1a4d4b5773fe4d3d
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-06-19 15:19:34 +0000

    Work on documentation.

--- chat/tests.py
+++ chat/tests.py
@@ -33,4 +33,4 @@ class ChatTests(ChannelsLiveServerTestCase):
             self._post_message('hello')
             WebDriverWait(self.driver, 2).until(lambda _:
                 'hello' in self._chat_log_value,
-                'Message was not receive
+                'Message was not received by window 1 from w
