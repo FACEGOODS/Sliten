ed20a06fe8eadf36a668b4d8adc10f50afbca09d
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-08-22 14:31:45 +0000

    add Dockerfile

--- chat/tests.py
+++ chat/tests.py
@@ -56,4 +56,5 @@ class ChatTests(ChannelsLiveServerTestCase):
 
             self._switch_to_window(1)
             self._post_message('world')
-            WebDriverWait(self.driver, 2).until(la
+            WebDriverWait(self.driver, 2).until(lambda _:
+     
