19b46d515bb0c8303bc721fde1c573a665855d98
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-08-23 13:53:49 +0000

    commented

--- chat/tests.py
+++ chat/tests.py
@@ -57,4 +57,4 @@ class ChatTests(ChannelsLiveServerTestCase):
             self._switch_to_window(1)
             self._post_message('world')
             WebDriverWait(self.driver, 2).until(lambda _:
-                'world' in se
+                'world' in self._chat_log_value
