2d9fd26984b2c3d53a104ac80c8752c79d979ccf
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-09-18 17:04:09 +0000

    Added google map xml file

--- chat/tests.py
+++ chat/tests.py
@@ -68,4 +68,4 @@ class ChatTests(ChannelsLiveServerTestCase):
 
     def _enter_chat_room(self, room_name):
         self.driver.get(self.live_server_url + '/chat/')
-        ActionChains(self.driver).s
+        ActionChains(self.driver).send_keys(room_
