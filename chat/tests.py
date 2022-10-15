78979c72a191b5fb568310dcff9aeb6af4024759
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-10-15 22:42:30 +0000

    Update module.json

--- chat/tests.py
+++ chat/tests.py
@@ -78,4 +78,5 @@ class ChatTests(ChannelsLiveServerTestCase):
 
     def _close_all_new_windows(self):
         while len(self.driver.window_handles) > 1:
-            self.driver.switch_to_window(self.driver.window_h
+            self.driver.switch_to_window(self.driver.window_handles[-1])
+  
