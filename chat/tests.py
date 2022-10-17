854aa7b7ef6bb20e14ace7365fb9be5942e9d1a7
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-10-17 10:45:44 +0000

    merge sprint

--- chat/tests.py
+++ chat/tests.py
@@ -79,4 +79,5 @@ class ChatTests(ChannelsLiveServerTestCase):
     def _close_all_new_windows(self):
         while len(self.driver.window_handles) > 1:
             self.driver.switch_to_window(self.driver.window_handles[-1])
-            self.driver.execute_script('wi
+            self.driver.execute_script('window.close();')
+      
