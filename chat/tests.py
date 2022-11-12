9158ab4cb8b9fa09f0705583d089f5121a85651e
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-11-12 13:14:58 +0000

    release 0.6

--- chat/tests.py
+++ chat/tests.py
@@ -87,4 +87,6 @@ class ChatTests(ChannelsLiveServerTestCase):
         self.driver.switch_to_window(self.driver.window_handles[window_index])
 
     def _post_message(self, message):
-        ActionChains(self.driver).send_keys(message + '\n'
+        ActionChains(self.driver).send_keys(message + '\n').perform()
+
+    @p
