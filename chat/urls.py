f8f9cabf67adfe739343eab88b813d2f261e8637
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-06 09:48:44 +0000

    usb:hub set hub->change_bits when over-current happens

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:room_name>/', views.room, name='r
+    path('<str:room_name>/', views.room, name='room'),
+    path('', views
