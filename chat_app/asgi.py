674ec92daf2b7d7b7ff1147d24ff44d98ae4b210
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-07 13:46:01 +0000

    Remove unnecessary tests

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -3,4 +3,5 @@ ASGI config for chat_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on 
+For more information on this file, see
+https://
