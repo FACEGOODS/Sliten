a64efd2eb4639ca8eb7ff608adf5eba0d70ea0c0
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-09 08:19:16 +0000

    Update FicheWeb.md

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -4,4 +4,4 @@ ASGI config for chat_app project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.c
+https://docs.djangoproject.com/en/3.2/howto
