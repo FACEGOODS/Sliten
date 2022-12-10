7cea8da7e04fea7fc4ff739a4dc3087f11f56751
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-10 08:04:58 +0000

    [RELEASE]merging 'master' into 'dev'

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -4,4 +4,6 @@ ASGI config for chat_app project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+i
