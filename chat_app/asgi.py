027d33d1be746b9e2e1290d0464a4704660b679e
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-10 11:58:18 +0000

    comments!

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -6,4 +6,5 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-i
+import os
+from 
