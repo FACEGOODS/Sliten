3adc59be7de3dbdfd900325ca8951b7be49e9142
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-18 13:47:52 +0000

    update patch

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -9,4 +9,4 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.
+from django.core.asgi import get_as
