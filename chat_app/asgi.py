7cab972d70d400b5ce21331ff4bbcf5a9281e71f
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-19 05:33:14 +0000

    updated unit tests

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -9,4 +9,5 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_as
+from django.core.asgi import get_asgi_application
+impor
