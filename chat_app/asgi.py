a01313b236f5ef11a75af8278178b5227493d36e
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-20 06:11:08 +0000

    Update Usage

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -12,4 +12,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.sett
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = 
