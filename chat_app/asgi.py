256556ae7a944dd9d0771cb485bf6f518aee15c2
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-21 17:30:24 +0000

    vendors

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -15,4 +15,5 @@ import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_appli
+    "http": get_asgi_application(),
+    "websocket":
