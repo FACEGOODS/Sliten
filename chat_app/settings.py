97abc9cf6377fd66427812342080dba071d91cd9
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-25 21:14:10 +0000

    hfsplus: fix bless ioctl when used with hardlinks

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -109,3 +109,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
