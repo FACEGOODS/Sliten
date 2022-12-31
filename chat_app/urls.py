f82be46afc34fce577d18c8adba7e6047738bb64
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-31 16:08:34 +0000

    Added package.json file

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -5,4 +5,5 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home'
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based view
