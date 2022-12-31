8ef06fb42dd3f64b109813353c691b142021a041
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-31 22:49:17 +0000

    Rename component

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -7,4 +7,4 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  
+    1. Add an import:  from other_app.v
