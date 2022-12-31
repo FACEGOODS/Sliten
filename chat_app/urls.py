86fa9b058546c4145dc129e538446cdb79741697
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-31 11:19:27 +0000

    Single GTFS data => Current (& Next) GTFS data

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -5,4 +5,4 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL 
+    2. Add a URL to urlpatterns
