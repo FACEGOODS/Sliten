9314f0a1374966d34c1d24f47e4d6fdc5f7e01b5
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2022-12-31 17:52:34 +0000

    tcp: incoming connections might use wrong route under synflood

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -6,4 +6,5 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based view
+Class-based views
+    1. Add an import:  
