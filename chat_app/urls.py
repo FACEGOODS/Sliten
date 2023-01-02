fc35cb8796b31efd8421e4f52883926b76e2dba9
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-01-02 19:05:20 +0000

    Updated the cf-autotick-bot-test-package feedstock.

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -8,4 +8,4 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to ur
+    2. Add a URL to urlpatterns:  path('', Home.
