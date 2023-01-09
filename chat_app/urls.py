bedd4f3dd38ed14af7f1e7f710cc6c85326981e1
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-01-09 09:08:26 +0000

    Run update-remote.sh on ruby

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -8,4 +8,5 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='hom
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another
