76b160f597c5883486a6de3de3cc8f505a42d359
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-01-10 13:49:42 +0000

    U.

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -9,4 +9,5 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another
+Including another URLconf
+    1. Import th
