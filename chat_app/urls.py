d7515c365f396da57702d28f370942d22352fca6
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-01-18 10:13:39 +0000

    update cronjoblog

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -14,4 +14,5 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import pat
+from django.urls import path
+from django.conf.ur
