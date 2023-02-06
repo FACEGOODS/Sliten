3ad9aed1dce18728b9a55f240f6fa62be2406d4e
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-02-06 21:47:48 +0000

    ftrace/jprobes/x86: Fix conflict between jprobes and function graph tracing

--- chat_app/wsgi.py
+++ chat_app/wsgi.py
@@ -3,4 +3,5 @@ WSGI config for chat_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on thi
+For more information on this file, see
+https://d
