1472ba0f54eb7c01b822ea78e05eec65e3499cca
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-02-07 09:04:45 +0000

    Corrections diverses

--- chat_app/wsgi.py
+++ chat_app/wsgi.py
@@ -4,4 +4,4 @@ WSGI config for chat_app project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.co
+https://docs.djangoproject.com/en/3.2/howto/d
