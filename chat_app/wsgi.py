416602f30915946a60dea8d57b226005fc49e1a9
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-02-08 13:41:32 +0000

    repository sync

--- chat_app/wsgi.py
+++ chat_app/wsgi.py
@@ -9,4 +9,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_applica
+from django.core.wsgi import get_wsgi_application
+
+os.envir
