72fc7d6a5801b258977fff274b40f2e9e7fc03d7
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-02-19 11:46:52 +0000

    firewire: ohci: fix JMicron JMB38x IT context discovery

--- manage.py
+++ manage.py
@@ -9,4 +9,4 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
     try:
         from django.core.management import execute_from_command_line
-  
+    except ImportE
