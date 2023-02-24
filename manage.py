4a6029b8e0cdbffe81d361f70f077f1b60cf74ed
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-02-24 17:55:06 +0000

    new web

--- manage.py
+++ manage.py
@@ -14,4 +14,5 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-     
+        ) from exc
+   
