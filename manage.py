4a31c9587e544b64293d35ff0150bcb2ba811cec
FACEGOODS <104331082+FACEGOODS@users.noreply.github.com>
2023-02-24 07:06:50 +0000

    having coffee

--- manage.py
+++ manage.py
@@ -13,4 +13,5 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtua
+            "forget to activate a virtual environment?"
+     
